faa227cce11a9309ce5ae57dc3f0b32d4965d1cd
Devildyw <1842501760@qq.com>
2022-06-20 00:31:07 +0800

    建造模式设计模式

---
+++ DesignPatterns-Java/BuilderPattern/src/main/java/com/dyw/Main.java
@@ -0,0 +1,11 @@
+package com.dyw;
+
+/**
+ * @author $USER
+ * @since ${YEAR}-${MONTH}-${DAY}-${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
